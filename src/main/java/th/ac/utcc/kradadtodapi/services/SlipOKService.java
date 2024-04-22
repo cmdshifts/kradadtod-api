acbd516ed2d37a024cb7fd3a973096a986f8e956
Akkharawit Somwong <akk.somwong@gmail.com>
2024-04-23 00:03:42 +0700

    update: SlipOk

--- src/main/java/th/ac/utcc/kradadtodapi/services/SlipOKService.java
+++ src/main/java/th/ac/utcc/kradadtodapi/services/SlipOKService.java
@@ -10,7 +10,7 @@ public class SlipOKService {
     public TransactionResponse fetchSlipOkData(String qrValue) {
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
-        headers.set("X-Authorization", "SLIPOKZGY4ZD7");
+        headers.set("X-Authorization", "SLIPOKEGP0L4Q");
         headers.setContentType(MediaType.APPLICATION_JSON);
 
         String requestBody = "{\"data\":\"" + qrValue + "\"}";
@@ -18,7 +18,7 @@ public class SlipOKService {
         HttpEntity<String> entity = new HttpEntity<>(requestBody, headers);
 
         ResponseEntity<TransactionResponse> response = restTemplate.exchange(
-                "https://api.slipok.com/api/line/apikey/19741",
+                "https://api.slipok.com/api/line/apikey/20443",
                 HttpMethod.POST,
                 entity,
                 TransactionResponse.class
